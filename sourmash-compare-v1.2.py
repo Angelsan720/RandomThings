b2692877505e7c2506d67556eeba45aef4405b94
Angel A. Sanquiche Sanchez <angelsan720@gmail.com>
2017-03-31 11:31:07 -0400

    Update sourmash-compare-v1.2.py

--- sourmash-compare-v1.2.py
+++ sourmash-compare-v1.2.py
@@ -1,3 +1,27 @@
+"""
+Angel A. Sanquiche Sanchez
+
+angelsan720@gmail.com
+angel.sanquiche@upr.edu
+"""
+
+"""
+This script is a command line tool if both screed and sourmash are in the PATH
+This script should also be able to be imported by anothe python script
+"""
+
+"""
+This little script takes two assembled transcriptomes A and B 
+For every sequence in A finds the sequence in B such that the jaccard of A and B is larger than T, T being 0.95 by default
+"""
+
+"""
+This script requires sourmash of UC Davis dib-lab https://github.com/dib-lab/sourmash
+This script requires screed of UC Davis dib-lab https://github.com/dib-lab/screed
+"""
+
+
+
 import time
 
 import sys
@@ -13,11 +37,14 @@ except ImportError:
 
 
 
-def usage():
-	print("Usage:\npython %s <file A> <file B>  "%(sys.argv[0]))
+def usage(use):
+	if ( use == "command" ):
+		print("Usage:\npython %s <file A> <file B>  "%(sys.argv[0]))
+	elif( use == "function" ):
+		print("Usage: s1=<file A> , s2=<file B>")
 	sys.exit(0)
 
-def compare_sequences(s1=None , s2=None):
+def compare_sequences(s1=None , s2=None , T=0.95):
 	#if imported
 	if s1 and s2:
 		seqfile1 = screed.open(s2)
@@ -67,8 +94,8 @@ def compare_sequences(s1=None , s2=None):
 	#for every sequence in file A
 	for seq1 in f1:
 
-		#set start asuming nothing matches
-		max = (None,None,0)
+		#set start asuming nothing matches and the minimum threshold
+		max = (None,None,T)
 
 		#for every sequence in file B
 		for seq2 in f2:
@@ -93,5 +120,5 @@ def compare_sequences(s1=None , s2=None):
 	print("output:%s-in-%.txts"%(sys.argv[1] , sys.argv[2]))
 
 if (__name__ == "__main__"):
-	compare_sequences()
+	compare_sequences(use="command")
 
