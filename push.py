14535c47d0dda09caeca38bf73e465e6d44a7bd7
Angel A. Sanquiche Sanchez <angelsan720@gmail.com>
2017-06-17 19:24:27 -0400

    Update push.py

--- push.py
+++ push.py
@@ -1,33 +1,49 @@
 #! /usr/bin/env python2
 
+"""
+Yeah I'm lazy
+Having to plug in a cable into my hpone to transfer junk like music got frustrating
+So I whipped this up to use Pushbullet to send things to my phone or browser
+Might adjust it to use another app depending if pushbullet doesnt work out long term
+"""
+"""
+Note this only partially works in python 3
+It works fine in python 2
+"""
+
 from pushbullet import Pushbullet
 import sys
 import ConfigParser
-""""""
-"""This is a command line tool to push documents to devices using the pushbulet api"""
-"""I use confige parser mainly because I have other things running using the config"""
-"""you need to rename push.py.config to 'config' and add your token to it"""
-"""This tool will list your devices and let you choose which to push it too"""
-
-
+import os
 def Usage():
         print("Usage: %s [file to upload 1] <file to upload 2> <file upload 3...>")
         sys.exit(0)
 def run():
         Config = ConfigParser.ConfigParser()
-        Config.read("config")
+        n = len(os.path.basename(__file__))
+        Config.read("%s/config"%(sys.argv[0][0:-n]))
         TOKEN = Config.get("Pushbullet" , "Token")
         pb = Pushbullet(TOKEN)
 
         print("getting devices:")
-        for i in range(0 , len(pb.devices)):
-                print(i , pb.devices[i])
-        d = input("\nselect a device:")
+        d = -1
+        while d not in range(0 , len(pb.devices)):
+                for i in range(0 , len(pb.devices)):
+                        print(i , pb.devices[i])
+                try:
+                        d = int(raw_input("\nselect a device:"))
+                except:
+                        pass
         phone = pb.devices[d]
 
         for i in range(1 , len(sys.argv)):
-                with open(sys.argv[i] , "rb") as f:
-                        file_data = pb.upload_file(f , sys.argv[i])
+                f = open(sys.argv[i] , "rb")
+                name = sys.argv[i]
+
+                q = raw_input("dDo you want to kee the name:'%s'?[YES/no]"%(sys.argv[i]))
+                if q == "no" or q == "No" or q == "n":
+                        name = raw_input("Enter a name for %s\n--->"%sys.argv[i])
+                file_data = pb.upload_file(f , name)
 
 
                 phone.push_file(**file_data)
